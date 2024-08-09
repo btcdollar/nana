3e776f7583fa5e0f1622981787674408a0e33367
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-08-09 22:30:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git scree
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-mkdir ~/ccminer
-cd ~/ccminer
+mkdir ccminer
+cd ccminer
 GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/CCminer-ARM-optimized/releases?per_page=1" | jq -c '[.[] | del (.body)]')
 GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
 GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
 
 echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 
-wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
+wget ${GITHUB_DOWNLOAD_URL} -O /ccminer/ccminer
 wget https://raw.githubusercontent.com/lukewrightmain/VerusCliMining/main/config.json -O ~/ccminer/config.json
-chmod +x ~/ccminer/ccminer
+chmod +x /ccminer/ccminer
 
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
