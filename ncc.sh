c4d2dfd3b10ba319989f77b0f39e388276f6a763
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-08-16 02:33:03 +0700

    Update ncc.sh

--- ncc.sh
+++ ncc.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+x=`ping -c1 google.com 2>&1 | grep unknown`
+if [ ! "$x" = "" ]; then
+        echo "It's down!! Attempting to restart."
+        su -c input keyevent 26
+        su -c svc wifi disable
+        su -c svc wifi enable
+fi
 
+result=`ps aux | grep -i "ccminer" | grep -v "grep" | wc -l`
+if [ $result -lt 2 ]
+   then
+        ~/ccminer/start.sh > /dev/null 2>&1
+fi
