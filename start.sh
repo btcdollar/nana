b0287cce799df9a9a6d4169b4087645ea70a8d11
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-08-16 02:24:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
-#!/data/data/com.termux/files/usr/bin/sh
-screen -S CCminer -X quit
-screen -wipe
-screen -dmS CCminer ~/ccminer/ccminer -c ~/ccminer/config.json
+#!/bin/sh
+#exit existing screens with the name CCminer
+screen -S CCminer -X quit 1>/dev/null 2>&1
+#wipe any existing (dead) screens)
+screen -wipe 1>/dev/null 2>&1
+#create new disconnected session CCminer
+screen -dmS CCminer 1>/dev/null 2>&1
+#run the miner
+screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
+printf '\nMining started.\n'
+printf '===============\n'
+printf '\nManual:\n'
+printf 'start: ~/.ccminer/start.sh\n'
+printf 'stop: screen -X -S CCminer quit\n'
+printf '\nmonitor mining: screen -x CCminer\n'
+printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
