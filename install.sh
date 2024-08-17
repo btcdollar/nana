94969ae1b55d04c8030e758168cbd38d00e14ce6
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-08-18 00:30:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 rm -rf ~/.termux && rm -rf ~/ccminer && rm -rf ~/.cache && rm ~/ncc.sh
-ssh_passwd='Passw0rd!'
 yes | pkg update
 yes | pkg upgrade
-yes | pkg install cronie termux-services libjansson wget nano screen nmap openssh
-yes ${ssh_passwd} | passwd u0_a118
-sshd
-mkdir -p ~/.termux/boot && mkdir -p ~/ccminer && mkdir ~/.cache
+yes | pkg install cronie termux-services libjansson wget nano screen nmap
+mkdir -p ~/.termux/boot
+mkdir -p ~/ccminer
+mkdir ~/.cache
 wget -q https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer -O ~/ccminer/ccminer
 wget -q https://raw.githubusercontent.com/btcdollar/nana/main/start.sh -O ~/ccminer/start.sh
 wget -q https://raw.githubusercontent.com/btcdollar/nana/main/config.json -O ~/ccminer/config.json
 wget -q https://raw.githubusercontent.com/btcdollar/nana/main/boot_start -O ~/.termux/boot/boot_start
 wget -q https://raw.githubusercontent.com/btcdollar/nana/main/ncc.sh -O ~/ncc.sh
-chmod +x ~/ccminer/ccminer && chmod +x ~/ccminer/start.sh && chmod +x ~/.termux/boot/boot_start && chmod +x ~/ncc.sh
+chmod +x ~/ccminer/ccminer
+chmod +x ~/ccminer/start.sh
+chmod +x ~/.termux/boot/boot_start
+chmod +x ~/ncc.sh
 (crontab -l 2>/dev/null; echo "*/5 * * * * ~/ncc.sh") | crontab -
 ~/ccminer/start.sh
 su -c reboot
