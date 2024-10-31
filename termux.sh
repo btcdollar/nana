bf9f84f14ff825b8529958f0f468deff6bde9634
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-10-31 15:37:48 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yes | pkg update
 yes | pkg upgrade
-yes | pkg install openssl libjansson automake build-essential clang lld curl git binutils
+yes | pkg install termux-services libjansson wget nano
 mkdir ~/ccminer && cd ~/ccminer
 wget -q https://raw.githubusercontent.com/btcdollar/nana/main/ccminer_a76 -O ~/ccminer/ccminer
 wget -q https://raw.githubusercontent.com/btcdollar/nana/main/config.json -O ~/ccminer/config.json
