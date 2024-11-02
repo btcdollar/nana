d57fd3f347aa427647e5ba2d638b9113d2b1fb7f
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-11-02 19:59:19 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -4,7 +4,7 @@ yes | pkg upgrade
 yes | pkg install termux-services libjansson wget nano
 mkdir ~/ccminer && cd ~/ccminer
 wget -q https://raw.githubusercontent.com/btcdollar/nana/main/start.sh -O ~/ccminer/start.sh
-wget -q https://raw.githubusercontent.com/btcdollar/nana/main/ccminer_a76 -O ~/ccminer/ccminer
+wget -q https://raw.githubusercontent.com/btcdollar/nana/refs/heads/main/ccminer_a53 -O ~/ccminer/ccminer
 wget -q https://raw.githubusercontent.com/btcdollar/nana/main/config.json -O ~/ccminer/config.json
 chmod +x ccminer start.sh
 cd && cd && cd
