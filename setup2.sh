34655610137b85ab0a91238b3f7f5fd23e482e66
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-11-25 13:08:54 +0700

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
-sudo apt-get update && sudo apt-get upgrade -y
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev ca-certificates openssh-sftp-server nano wget
+wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+if [ ! -d ~/.ssh ]
+then
+  mkdir ~/.ssh
+  chmod 0700 ~/.ssh
+  cat << EOF >> ~/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOAPjnQbztXVnQDGl1Xqty7kFyTZY4MEJgCn6Et6vRpivHV+dMHyLnsCp2iAxDnh99ld9P8adVpwWdOaaUm3wzxzRFK+83ZZGGuFO6/BRzC8J0D4CFur++rVdC8zHhyaGKTVU/I10dpYwVfnfyYjhBoFhb8lnMp3SIkd56HnHJDuAWg6Cr31wvevUbulOHh6uOaBdvyyAJxY8ASHyPOEDDIlo+4/oirMudO0L46zTvxktmAby0ZwD4jOcUK2K52L0+Xe7zWBRnDtY9bKTLoJFQ9McSVzoVTsXxlvGIJmW0WGTPfLf/o3w5T+JiE1vJL2/S+A7YSMgo952fofGl6r6R
+EOF
+  chmod 0600 ~/.ssh/authorized_keys
+fi
 
-sudo apt-get install software-properties-common git nano -y
-
-wget https://apt.llvm.org/llvm.sh
-
-chmod +x llvm.sh && sudo ./llvm.sh 16
-
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
-
-sudo apt-get install -y libllvm-16-ocaml-dev libllvm16 llvm-16 llvm-16-dev llvm-16-doc llvm-16-examples llvm-16-runtime clang-16 clang-tools-16 clang-16-doc libclang-common-16-dev libclang-16-dev libclang1-16 clang-format-16 python3-clang-16 clangd-16 clang-tidy-16 libclang-rt-16-dev libpolly-16-dev libfuzzer-16-dev lldb-16 lld-16 libc++-16-dev libc++abi-16-dev libomp-16-dev libclc-16-dev libunwind-16-dev libmlir-16-dev mlir-16-tools flang-16 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64
-
-sudo ln -sf /usr/lib/llvm-16/bin/clang-16 /usr/bin/clang
-
-sudo ln -sf /usr/lib/llvm-16/bin/clang++ /usr/bin/clang++
-
-git clone https://github.com/btcdollar/ccminer.git
-
-cd ccminer
-
-chmod +x build.sh configure.sh autogen.sh
-
-CXX=clang++ CC=clang ./build.sh
-
-./ccminer -c config.json
+if [ ! -d ~/ccminer ]
+then
+  mkdir ~/ccminer
+fi
+sudo wget -q https://raw.githubusercontent.com/btcdollar/nana/refs/heads/main/start.sh -O /etc/profile.d/ccminer.sh
+sudo chmod +x /etc/profile.d/ccminer.sh
+cd ~/ccminer
+wget -q https://raw.githubusercontent.com/btcdollar/nana/refs/heads/main/user_ccminer_a53 -O ~/ccminer/ccminer
+wget -q https://raw.githubusercontent.com/btcdollar/nana/main/config.json -O ~/ccminer/config.json
+chmod +x ~/ccminer/ccminer
+sleep 2
+exit
+sleep 2
+su -c reboot
