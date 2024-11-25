c79746ae907dd4abe74a56258093ce0dca4de55e
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-11-25 13:06:24 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev ca-certificates openssh-sftp-server nano wget
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 if [ ! -d ~/.ssh ]
 then
