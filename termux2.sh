5e4f2616b50a6d83e0738a10fe4e3f8ed30b64b4
Daniel <163345545+btcdollar@users.noreply.github.com>
2025-05-15 03:54:28 +0700

    Create termux2.sh

---
+++ termux2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yes | pkg update
+yes | pkg upgrade
+yes | pkg install termux-services libjansson wget nano
+rm -rf ~/ccminer
+rm -rf ~/.ssh
+mkdir ~/ccminer && cd ~/ccminer
+wget -q https://raw.githubusercontent.com/btcdollar/nana/main/start.sh -O ~/ccminer/start.sh
+wget -q https://raw.githubusercontent.com/btcdollar/nana/refs/heads/main/ccminer_a53 -O ~/ccminer/ccminer
+wget -q https://raw.githubusercontent.com/btcdollar/nana/main/config.json -O ~/ccminer/config.json
+chmod +x ccminer start.sh
+cd && cd && cd
+sed -i -e '$acd ccminer/&&./start.sh' ../usr/etc/bash.bashrc
